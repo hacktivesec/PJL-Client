998e72930f8cf78dffdd16ebd33617a8965e6f50
hacktivesec <info@hacktivesecurity.com>
2017-04-26 17:11:22 +0200

    Update pjlclient.py

--- pjlclient.py
+++ pjlclient.py
@@ -20,11 +20,11 @@ def conn(string,host):
      
 def cmdlist(cmd,host,path):
     if sys.argv[2] == 'dir':
-        cmd = '\x1b%-12345X@PJL FSDIRLIST NAME="0:' + path + '" ENTRY=1 COUNT=999999\x0d\x0a\x1b%-12345X\x0d\x0a'
+        cmd = '\x1b%-12345X@PJL FSDIRLIST NAME="0:' + path + '" ENTRY=1 COUNT=999999\x0d\x0a'
         conn(cmd,host)
     elif sys.argv[2] == 'read':
         try:
-            cmd = '\x1b%-12345X@PJL FSUPLOAD NAME="0:'+ path +'" OFFSET=0 SIZE=999999\x0d\x0a\x1b%-12345X\x0d\x0a'
+            cmd = '\x1b%-12345X@PJL FSUPLOAD NAME="0:'+ path +'" OFFSET=0 SIZE=999999\x0d\x0a'
             conn(cmd,host)
         except:
             print 'Select an existent path'
